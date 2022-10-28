143edc2e7fdd9c83d1a0c8f32e5be2ee26c2ed39
Rajapandi <92912777+Rajapandi1212@users.noreply.github.com>
2022-10-28 13:45:50 +0530

    Basic font,color added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import "./App.css";
 
 const App = () => {
-  return <div>Guidanzz</div>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline primary_container primary_bg_gradient font-[gilroy-regular]">
+        Guidanz
+      </h1>
+    </>
+  );
 };
 
 export default App;
