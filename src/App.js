f432bfb9758b49a24d81c628eb11345edc702f70
Dineshdk555 <104187302+Dineshdk555@users.noreply.github.com>
2022-11-03 17:34:38 +0530

    Plp Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "./component/header";
+import Footer from "./component/footer/Index.js";
 import "./App.css";
+import PlpList from "./component/algolia/custom/PlpList";
 
 const App = () => {
   return (
     <>
       <Header />
+      <PlpList />
+      <Footer />
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
+import "instantsearch.css/themes/satellite.css";
 import "@algolia/autocomplete-theme-classic";
 import "./index.css";
 import App from "./App";
-import Index from "./component/Footer/Index";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -12,7 +12,6 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
-      <Index />
     </BrowserRouter>
   </React.StrictMode>
 );
