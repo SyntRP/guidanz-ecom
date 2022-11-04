c5c5e73589ae7c2c76462ba43f3e603be47dc987
Rajapandi <92912777+Rajapandi1212@users.noreply.github.com>
2022-11-04 19:23:54 +0530

    footer added

---
+++ src/component/footer/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import FooterContent from "./FooterContent";
+import FooterCpyRights from "./FooterCpyRights";
+import FooterHeader from "./FooterHeader";
+
+const Footer = () => {
+  return (
+    <div>
+      <FooterHeader />
+      <FooterContent />
+      <FooterCpyRights />
+    </div>
+  );
+};
+
+export default Footer;
