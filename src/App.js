35cc26851c180544f2028548e7180e164194b2d2
Dineshdk555 <104187302+Dineshdk555@users.noreply.github.com>
2022-11-07 20:25:04 +0530

    fixed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Header from "./component/header";
 import Homepage from "./page/homepage";
-import Footer from "./component/footer/Index";
+import Footer from "./component/footer/index";
 import PlpList from "./page/PlpList";
 import PDP from "./page/PDP";
 import "./App.css";
